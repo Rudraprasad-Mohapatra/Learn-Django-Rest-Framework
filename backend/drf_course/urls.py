27f69cdaba6534db6c39805b42690bf23643c4cd
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2025-10-25 23:53:14 +0530

    Module 3 completed

--- backend/drf_course/urls.py
+++ backend/drf_course/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib import admin
 from rest_framework import routers
+from core import views as core_views
 
 router = routers.DefaultRouter()
 
@@ -8,4 +9,5 @@ urlpatterns = router.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contact/', core_views.ContactAPIView.as_view()),
 ]
