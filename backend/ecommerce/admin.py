1247b56dbd349ec043b20a5268c34a2127b039a1
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2025-11-07 23:12:16 +0530

    Item & Order Models migrations pushed & admin codes pushed with marking end of Module-6

--- backend/ecommerce/admin.py
+++ backend/ecommerce/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'stock')
+    search_fields = ('title',)
+    list_filter = ('price','stock')
+    readonly_fields = ('id',)
+    ordering = ('title',)  # sorts alphabetically
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'item', 'quantity')
+    search_fields = ('item__title',)
+    list_filter = ('item',)
+    ordering = ('id',)
